d56de9919e84926ffc79b2873f85aeb02d3108f8
Jakub Juřena <445319@mail.muni.cz>
2018-03-16 16:44:02 +0100

    implemented basic functionality

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+let users = [];
+
+io.on("connection", function(socket) {
+  console.log("User is connected.");
+
+  socket.on("setUsername", function(data) {
+    console.log("Try set username: " + JSON.stringify(data));
+
+    if (users.indexOf(data.username) > -1) {
+      socket.emit("usernameExists", {
+        type: "error",
+        message: data.username + " username is taken! Try some other username."
+      });
+    } else {
+      users.push(data.username);
+      socket.emit("usernameSet", { type: "success", username: data.username });
+    }
+  });
+
+  socket.on("sendMessage", function(data) {
+    console.log("Message: " + JSON.stringify(data));
+    io.sockets.emit("newMessage", data);
+  });
+});
+
 http.listen(80, function(err) {
   if (err) throw Error("Cannot start server on port 80");
   console.log("Listening on port 80");
